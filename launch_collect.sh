b1d40938bebd37bc92e00eaf13255dfcd89873af
Jordi Armengol Estapé <me@jordiae.com>
2019-11-18 17:12:30 +0100

    removed .gz filename extension in launch script

--- launch_collect.sh
+++ launch_collect.sh
@@ -2,6 +2,6 @@
 PYTHON=python3
 SCRIPT=collect_sentences.py
 
-for file_pair in "pubmed19n0001.xml.gz pubmed19n0139.xml.gz" "pubmed19n0140.xml.gz pubmed19n0278.xml.gz" "pubmed19n0279.xml.gz pubmed19n0417.xml.gz" "pubmed19n0418.xml.gz pubmed19n0556.xml.gz" "pubmed19n0557.xml.gz pubmed19n0695.xml.gz" " pubmed19n0696.xml.gz pubmed19n0834.xml.gz" "pubmed19n0835.xml.gz pubmed19n0972.xml.gz";  do
+for file_pair in "pubmed19n0001.xml pubmed19n0139.xml" "pubmed19n0140.xml pubmed19n0278.xml" "pubmed19n0279.xml pubmed19n0417.xml" "pubmed19n0418.xml pubmed19n0556.xml" "pubmed19n0557.xml pubmed19n0695.xml" " pubmed19n0696.xml pubmed19n0834.xml" "pubmed19n0835.xml pubmed19n0972.xml";  do
   $PYTHON $SCRIPT $file_pair &
 done
