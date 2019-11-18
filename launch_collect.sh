18eae7358d60d2efee5fe73d055e2d30f46a3bb3
Jordi Armengol Estapé <me@jordiae.com>
2019-11-18 17:52:27 +0100

    python3.7

--- launch_collect.sh
+++ launch_collect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PYTHON=python3
+PYTHON=python3.7
 SCRIPT=collect_sentences.py
 
 for file_pair in "pubmed19n0001.xml pubmed19n0139.xml" "pubmed19n0140.xml pubmed19n0278.xml" "pubmed19n0279.xml pubmed19n0417.xml" "pubmed19n0418.xml pubmed19n0556.xml" "pubmed19n0557.xml pubmed19n0695.xml" " pubmed19n0696.xml pubmed19n0834.xml" "pubmed19n0835.xml pubmed19n0972.xml";  do
